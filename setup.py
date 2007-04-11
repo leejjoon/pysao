6ccb5da354249a7b447f21ed775f95d5857a5135
lee.j.joon <lee.j.joon@36f92cbc-c624-0410-b750-9df39f72c663>
2007-04-11 08:43:04 +0000

    setup.py : slight changes
    git-svn-id: https://python-sao.googlecode.com/svn/trunk@17 36f92cbc-c624-0410-b750-9df39f72c663

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Pyrex.Distutils import build_ext
+#from Pyrex.Distutils import build_ext
 
 import sys
  
-if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
-    raise SystemExit, "Python 2.3 or later required to build pysao."
+#if not hasattr(sys, 'version_info') or sys.version_info < (2,3,0,'alpha',0):
+#    raise SystemExit, "Python 2.3 or later required to build pysao."
 
 def dolocal():
     """Adds a command line option --local=<install-dir> which is an abbreviation for
@@ -33,12 +33,12 @@ def main():
           platforms = ["Linux","Solaris","Mac OS X"],
           packages = ['pysao'],
           package_dir={'pysao':'lib'},
-          ext_modules=[ Extension("xpa",       ["xpa.pyx"],
-                                  include_dirs=['/Users/jjlee/local/src/xpalib/xpa-2.1.6'],
-                                  library_dirs=['/Users/jjlee/local/src/xpalib/xpa-2.1.6'],
+          ext_modules=[ Extension("xpa",       ["xpa.c"],
+                                  include_dirs=['./xpalib'],
+                                  library_dirs=['./xpalib'],
                                   libraries=['xpa']),
                         ],
-          cmdclass = {'build_ext': build_ext},
+          #cmdclass = {'build_ext': build_ext},
           #test_suite = "test.saods9_test",
           )
 
