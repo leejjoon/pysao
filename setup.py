4803a1e4382994ce27bcec84ebcef3ef89a204d4
lee.j.joon <lee.j.joon@36f92cbc-c624-0410-b750-9df39f72c663>
2009-04-08 03:45:24 +0000

    initial documentation
    git-svn-id: https://python-sao.googlecode.com/svn/trunk@106 36f92cbc-c624-0410-b750-9df39f72c663

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from distutils.core import setup
 from distutils.extension import Extension
-#from Pyrex.Distutils import build_ext
 
 import sys
  
@@ -11,20 +10,20 @@ def main():
           version = "0.1b1",
           description = "python wrapper around some SAO tools",
           author = "Jae-Joon Lee",
+          author_email = "lee.j.joon@gmail.com",
           maintainer_email = "lee.j.joon@gmail.com",
-          license = "???",
+          url = "",
+          license = "MIT",
           platforms = ["Linux","Mac OS X"], # "Solaris"?
           packages = ['pysao'],
           package_dir={'pysao':'lib'},
-          package_data={'pysao': ["ds9_xpa_help.pickle"]},
+          #package_data={'pysao': ["ds9_xpa_help.pickle"]},
           
           ext_modules=[ Extension("pysao.xpa",       ["xpa.c"],
                                   include_dirs=['./xpalib'],
                                   library_dirs=['./xpalib'],
                                   libraries=['xpa']),
                         ],
-          #cmdclass = {'build_ext': build_ext},
-          #test_suite = "test.saods9_test",
           )
 
 if __name__ == "__main__":
