ce2be0446fb2a66c981e30e5cde3de9aa64a3324
Jae-Joon Lee <lee.j.joon@gmail.com>
2012-05-15 21:47:57 +0900

    new setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,77 @@ use_setuptools()
 
 from setuptools import setup, Extension
 
-#from distutils.core import setup
-#from distutils.extension import Extension
+# check if cython or pyrex is available.
+pyrex_impls = 'Cython.Distutils.build_ext', 'Pyrex.Distutils.build_ext'
+for pyrex_impl in pyrex_impls:
+    try:
+        # from (pyrex_impl) import build_ext
+        build_ext = __import__(pyrex_impl, fromlist=['build_ext']).build_ext
+        break
+    except:
+        pass
+have_pyrex = 'build_ext' in globals()
 
-#import sys
+if have_pyrex:
+    PYREX_SOURCE = "src/_region_filter.pyx"
+else:
+    PYREX_SOURCE = "src/_region_filter.c"
+
+import os.path
+XPALIB_DIR = "xpa-2.1.13"
+
+xpalib_files = """xpa.c xpaio.c command.c acl.c remote.c
+                  clipboard.c port.c tcp.c client.c word.c
+                  xalloc.c find.c xlaunch.c timedconn.c
+                  """.split()
+
+xpa_sources = ["xpa.pyx"]  + [os.path.join(XPALIB_DIR, c) \
+                              for c in open("files.txt").read().split()]
+
+xpalib_defines  = [(s, "1") for s in """HAVE_STRING_H
+                                        HAVE_STDLIB_H
+                                        HAVE_MALLOC_H
+                                        HAVE_UNISTD_H
+                                        HAVE_GETOPT_H
+                                        HAVE_MEMCPY
+                                        HAVE_PWD_H
+                                        HAVE_VALUES_H
+                                        HAVE_DLFCN_H
+                                        HAVE_SYS_UN_H
+                                        HAVE_SYS_SHM_H
+                                        HAVE_SYS_MMAN_H
+                                        HAVE_SYS_IPC_H
+                                        HAVE_SETJMP_H
+                                        HAVE_SOCKLEN_T
+                                        HAVE_STRCHR
+                                        HAVE_MEMCPY
+                                        HAVE_SNPRINTF
+                                        HAVE_SETENV""".split()]
+
+
+
+# this may not likely the best way to do.
+
+class build_ext_withxpalib(build_ext):
+    def build_xpalib(self):
+        import subprocess, sys
+        if sys.platform=='darwin':
+            cflags = "CFLAGS=-fPIC -O2 -force_cpusubtype_ALL -arch ppc -arch i386 -arch x86_64"
+        else:
+            cflags = "CFLAGS=-fPIC -O2"
+        subprocess.check_call(["./configure", cflags], cwd=XPALIB_DIR)
+        subprocess.check_call(["make", "clean"], cwd=XPALIB_DIR)
+        subprocess.check_call(["make"], cwd=XPALIB_DIR)
+
+    def run(self):
+        self.build_xpalib()
+        build_ext.run(self)
 
 
 def main():
     #dolocal()
     setup(name = "pysao",
-          version = "0.1b1",
+          version = "0.1b2",
           description = "python wrapper around some SAO tools",
           author = "Jae-Joon Lee",
           author_email = "lee.j.joon@gmail.com",
@@ -24,12 +85,15 @@ def main():
           package_dir={'pysao':'lib'},
           #package_data={'pysao': ["ds9_xpa_help.pickle"]},
 
-          ext_modules=[ Extension("pysao.xpa",       ["xpa.pyx"],
-                                  include_dirs=['./xpalib'],
-                                  library_dirs=['./xpalib'],
-                                  libraries=['xpa']),
+          ext_modules=[ Extension("pysao.xpa", xpa_sources,
+                                  include_dirs=[XPALIB_DIR],
+                                  define_macros = xpalib_defines,
+                                  #library_dirs=[XPALIB_DIR],
+                                  #libraries=['xpa']
+                                  ),
                         ],
-          #use_2to3 = True,
+          #cmdclass = {'build_ext': build_ext_withxpalib},
+          use_2to3 = True,
           )
 
 if __name__ == "__main__":
